254f49140c5018c427de7577d0c775fac9eaa808
PriyaRNair27 <priyarnair2710@gmail.com>
2022-05-18 10:34:18 +0530

    newss

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Viewproduct from './Components/Viewproduct';
 
 function App() {
   return (
     <div>
-
+    <Viewproduct/>
     </div>
   );
 }
